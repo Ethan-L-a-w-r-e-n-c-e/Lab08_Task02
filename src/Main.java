b3fedc30e4cff658a8e9af7ad6a7fc078eeaec05
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-11-04 08:23:01 -0500

    v

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int favoriteInt = InputHelper.getInt(scan,"What is your favorite Integer");
+        System.out.println("Your favorite integer is : " + favoriteInt);
     }
 }
